b49dfe9a1834895d09e72c1f15eb9c95568b4541
William Heyman Krill <whk@magenta.dk>
2018-07-11 10:12:51 +0200

    Index-views for servers, systems, and customers.
    Could use some refactorisation and mayby an accordion is not the perfect solution. Small fixes elsewhere: the base  template has been fixed with a title block and urls has been changed to accept plural s in the index views.

--- magentadriftinfo/urls.py
+++ magentadriftinfo/urls.py
@@ -5,10 +5,11 @@ from . import views
 app_name = 'magentadriftinfo'
 
 urlpatterns = [
-    url(r'^customer/$', views.CustomerIndexView.as_view(), name='customer_index'),
-    url(r'^system/$', views.SystemIndexView.as_view(), name='system_index'),
-    url(r'^server/$', views.ServerIndexView.as_view(), name='server_index'),
     url(r'^$', views.DetailView.as_view(), name='detail'),
+    url(r'^index/$', views.IndexView.as_view(), name='index'),
+    url(r'^customers?/$', views.CustomerIndexView.as_view(), name='customer_index'),
+    url(r'^systems?/$', views.SystemIndexView.as_view(), name='system_index'),
+    url(r'^servers?/$', views.ServerIndexView.as_view(), name='server_index'),
     url(r'^server/(?P<server>[a-zA-Z0-9 ]+)/$', views.DetailView.as_view(), name='server'),
     url(r'^system/(?P<system>[a-zA-Z0-9 ]+)/$', views.DetailView.as_view(), name='system'),
     url(r'^customer/(?P<customer>[a-zA-Z0-9 ]+)/$', views.DetailView.as_view(), name='customer'),
