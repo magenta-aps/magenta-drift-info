a2362bbc79e5643d310630bf1d436135443cb9b4
William Heyman Krill <whk@magenta.dk>
2019-05-08 15:07:24 +0200

    Fix settings.py

--- magentadriftinfosite/settings.py
+++ magentadriftinfosite/settings.py
@@ -19,11 +19,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'g!lf^*mx#ufl$&rrm7i_p9o45)fa!=5j5$o#^_=j0d&qd#2j3e'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+X_FRAME_OPTIONS = 'DENY'
 
 ALLOWED_HOSTS = []
 
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'simple_history',    # https://django-simple-history.readthedocs.io/en/latest/usage.html
+    'simple_history',    # https://django-simple-history.readthedocs.io
     'magentadriftinfo.apps.MagentadriftinfoConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'simple_history.middleware.HistoryRequestMiddleware',   # Maybe not needed
+    'simple_history.middleware.HistoryRequestMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -121,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOCAL_SETTINGS_FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)), "local_settings.py")
+if os.path.exists(LOCAL_SETTINGS_FILE):
+    from .local_settings import *  # noqa
